f4252034437cf3f494dfef75185632f78b17917a
Alexandre <alexandre.padellini@telecomnancy.eu>
2025-12-03 11:47:14 +0100

    Fetch puzzle inputs automatically

--- auto_setup.sh
+++ auto_setup.sh
@@ -6,11 +6,14 @@ dir="day${day}"
 mkdir -p "$dir"
 
 # Copy and create files
-touch "${dir}/input.txt"
 touch "${dir}/test_input.txt"
 cp "template.py" "${dir}/silver.py"
 touch "${dir}/gold.py"
 cp "template.md" "${dir}/comments.md"
+
+# Fetch and save input
+token=$(cat AoC_token.txt)
+curl -b "session=${token}" "https://adventofcode.com/2025/day/$1/input" > "${dir}/input.txt"
 
 # Edit day number in python script
 sed -i "s|\"InsertDayNumberHere\"|$1|" "${dir}/silver.py"
