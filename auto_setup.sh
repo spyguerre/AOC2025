8377fb01bad2cc6dea8f52a247d54008d5a3f91d
Alexandre <alexandre.padellini@telecomnancy.eu>
2025-12-03 12:45:32 +0100

    Also automatically fetch and extract example input

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+year=2025
+day=$1
+
+token=$(cat AoC_token.txt)
+dir="day$(printf "%02d\n" "${day}")"
+
 # Create dir
-day=$(printf "%02d\n" "$1")
-dir="day${day}"
-mkdir -p "$dir"
+mkdir -p "${dir}"
 
 # Copy and create files
-touch "${dir}/test_input.txt"
 cp "template.py" "${dir}/silver.py"
 touch "${dir}/gold.py"
 cp "template.md" "${dir}/comments.md"
 
 # Fetch and save input
-token=$(cat AoC_token.txt)
-curl -b "session=${token}" "https://adventofcode.com/2025/day/$1/input" > "${dir}/input.txt"
+curl -sb "session=${token}" "https://adventofcode.com/${year}/day/${day}/input" > "${dir}/input.txt"
+
+# Fetch, scrap and save test input (aka, the biggest code snippet inside the subject page)
+curl -sb "session=${token}" "https://adventofcode.com/${year}/day/${day}" | sed -n '/<code>/,/<\/code>/p' | awk 'BEGIN{RS="</code>"; FS="<code>"} { gsub(/\r/,""); block=$2; sub(/\n$/,"",block); if(length(block)>max){max=length(block); s=block} } END{print s}' > "${dir}/test_input.txt"
 
 # Edit day number in python script
-sed -i "s|\"InsertDayNumberHere\"|$1|" "${dir}/silver.py"
+sed -i "s|\"InsertDayNumberHere\"|${day}|" "${dir}/silver.py"
 
 # Activate python venv
 source .venv/bin/activate
